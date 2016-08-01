23f8b5e399a917b9f293c28b4cdaca4884624b82
flai78 <fililai@gmail.com>
2016-08-02 00:57:05 +0200

    Add login

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -3,11 +3,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'login',
-    templateUrl: './pages/login/login.component.html'
+    templateUrl: 'pages/login/login.component.html',
+    styleUrls:["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class LoginComponent implements OnInit {
-    constructor() { }
+    
+    public username: String;
+    public password: String;
+
+    constructor() {
+     this.username = new String();
+     this.password = new String();
+        
+     }
 
     ngOnInit() { }
 
+    onTap(){
+        alert(  this.username +" " + this.password);
+    }
 }
+
