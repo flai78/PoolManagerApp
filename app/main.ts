630a16b07eb5e2b202955c1e8c58dc896e259b7b
flai78 <fililai@gmail.com>
2016-08-01 22:46:23 +0200

    Add styles

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {HomeComponent} from "./pages/home/home";
+import { LoginComponent } from './pages/login/login.component';
 
-nativeScriptBootstrap(HomeComponent);
+nativeScriptBootstrap(LoginComponent);
