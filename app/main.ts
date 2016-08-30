30633d63cb4d0d312a92e017d878251e23b8856e
flai78 <fililai@gmail.com>
2016-08-30 20:39:06 +0200

    Update to new ns version

--- app/main.ts
+++
@@ -1,6 +0,0 @@
-// this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { nativeScriptBootstrap } from "nativescript-angular/application";
-import { HomeComponent } from "./pages/home/home";
-import { LoginComponent } from './pages/login/login.component';
-
-nativeScriptBootstrap(LoginComponent);
